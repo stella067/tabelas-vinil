fca382d2988e57e4fec8dd5af87f0b9ef58cb890
stella067 <161174048+stella067@users.noreply.github.com>
2025-07-08 11:59:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 
  //VOCÊ DEVE SUBSTITUIR OS CÓDIGOS ABAIXOS CÓDIGOS DO SEU PROJETO NO FIREBASE (FIREBASE CONFIGURATION)
- // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAAlNNWN6zmeytGfHwVKC34Vei0g2UDpD8",
   authDomain: "gaby5555.firebaseapp.com",
@@ -12,7 +11,6 @@ const firebaseConfig = {
   appId: "1:60126292567:web:309dc641ce5c26bb8b5112",
   measurementId: "G-XLXSHCYYK2"
 };
- 
 
  // Initialize Firebase
  const app = initializeApp(firebaseConfig);
